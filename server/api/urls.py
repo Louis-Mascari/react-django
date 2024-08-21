6a777dc76bb6935f9a526f3f614b42558c8e8178
Louis-Mascari <Lmascari16@gmail.com>
2024-08-21 11:02:08 -0400

    sets up django backend

---
+++ server/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("recipes/", views.RecipeListCreate.as_view(), name="recipe-view-create"),
+    path("recipes/<int:pk>/",
+         views.RecipeRetrieveUpdateDestroy.as_view(), name="update")
+]
---
+++ server/api/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Recipe
+from .serializers import RecipeSerializer
+
+
+class RecipeListCreate(generics.ListCreateAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+
+class RecipeRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    lookup_field = "pk"
